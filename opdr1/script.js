300e714156528a4552135adb4a59ff64567f2f0b
SoufianeLoukan <71612439+SoufianeLoukan@users.noreply.github.com>
2021-06-14 06:50:16 +0200

    Add files via upload

---
+++ opdr1/script.js
@@ -0,0 +1,18 @@
+console.log(5 + 6)
+var a = 10
+var mijnauto = {
+    merk: "ford",
+    type: "mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+if (a > 9) {
+    console.log("hallo a is groter dan 9 hoor")
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log("i is nu " + i);
+}
+
+console.log(mijnauto.kleur);
